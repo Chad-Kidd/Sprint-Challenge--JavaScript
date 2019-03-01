e1b5570eeed4043f34b0cb0c683f235da00f3201
chad kidd <ckiddwebdev@gmail.com>
2019-03-01 09:49:34 -0800

    functions

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,4 +1,4 @@
-// ==== Callbacks ====  
+// / ==== Callbacks ====  
 
 /* Step 1: Create a higher-order function that accepts a callback
   * Create a higher-order function named consume that can take 3 parameters.
@@ -6,19 +6,27 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function consume(param1, param2, hollaback) {
+   return hollaback(param1, param2); }
+  //"Aint no hollaback function" "Actually, you are."
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add (num1, num2) {return num1 + num2;}
+function multiply(num1, num2) {return num1 * num2;}
 
+function greeting(firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`
+}
 
-/* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+/* Step 3: Check your work by uncommenting the following calls to consume(): */
+console.log(consume(2,2,add));
+console.log(consume(10,16,multiply));
+console.log(consume("Mary","Poppins", greeting));
+// Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,6 +34,7 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
+// A nested function has access to the "workspaces" of all functions in which it is nested, as well as it's own. 
 
 
 const external = "I'm outside the function";
