e0bd647d13b3f7e56676025665f2d9a1f3049f79
chad kidd <ckiddwebdev@gmail.com>
2019-03-01 10:01:14 -0800

    dino objects-arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -6,30 +6,53 @@
   object name, diet, weight, length, period
 */
 
+const dino1 = {
+  name: "tyrannosaurus",
+  diet: "carnivorous",
+  weight: "7000kg",
+  length: "12m",
+  color: "purple",
+  period: "Cretaceious",
+};
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
 
+const dino2 = {
+  name: "stegosaurus",
+  diet: "herbivorous",
+  weight: "2000kg",
+  length: "9m",
+  period: "Late Jurassic",
+};
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+const dino3 = {
+  name: "velociraptor",
+  diet: "carnivorous",
+  weight: "15kg",
+  length: "1.8m",
+  period: "Late Cretaceious",
+};
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
+
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(dino1.name);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(dino3.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(dino2.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
-
+console.log(dino1.period);
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+roar = () => `The ${dino1.color} ${dino1.name} goes RAWERSRARARWERSARARARRRR!`;
 
+console.log(roar());
 
 // ==== Arrays ====
 
